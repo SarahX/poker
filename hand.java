edb9ead5c2c1b77d625e66bbbc723b77adea802d
iolive12 <56235419+iolive12@users.noreply.github.com>
2019-10-30 23:56:50 -0400

    Update hand.java

--- hand.java
+++ hand.java
@@ -12,7 +12,14 @@ public class Hand {
 		for (int i = 5; i < 10; i++) {
 			hand2[i - 5] = d.drawFromDeck(i); //fill up 2nd hand array.
 		}
+		analyze(hand1, val1);
+		display(val1);
+		analyze(hand2, val2);
+		display(val2);
 
+	}
+
+	public void analyze(Card[] hand, int[] val) {	
 		int[] ranks = new int[14];
 		int[] orderedRanks = new int[5];	 //miscellaneous cards that are not otherwise significant
 		boolean flush = true;
@@ -28,10 +35,10 @@ public class Hand {
 			ranks[i]=0;
 		}
 		for (int i = 0; i <= 4; i++) {
-			ranks[cards[i].getRank()]++;
+			ranks[hand[i].getRank()]++;
 		}
 		for (int i = 0; i < 4; i++) {
-			if (cards[i].getSuit() != cards[i + 1].getSuit()) {
+			if (hand[i].getSuit() != hand[i + 1].getSuit()) {
 				flush = false;
 			}
 		}
@@ -80,124 +87,112 @@ public class Hand {
 			topStraightValue = 14; //higher than king
 		}
 
-		for (int i = 0; i <= 5; i++){
-			value[i]=0;
+		for (int i = 0; i <= 5; i++){	//clearing val after using it to get info
+			val[i] = 0;
 		}
 
 
 		//start hand evaluation
-		if ( sameCards==1 ) {
-			value[0]=1;
-			value[1]=orderedRanks[0];
-			value[2]=orderedRanks[1];
-			value[3]=orderedRanks[2];
-			value[4]=orderedRanks[3];
-			value[5]=orderedRanks[4];
+		if (sameCards == 1) {
+			val[0] = 1;
+			val[1] = orderedRanks[0];
+			val[2] = orderedRanks[1];
+			val[3] = orderedRanks[2];
+			val[4] = orderedRanks[3];
+			val[5] = orderedRanks[4];
 		}
 
-		if (sameCards==2 && sameCards2==1)
-		{
-			value[0]=2;
-			value[1]=largeGroupRank; //rank of pair
-			value[2]=orderedRanks[0];
-			value[3]=orderedRanks[1];
-			value[4]=orderedRanks[2];
+		if (sameCards == 2 && sameCards2 == 1) {
+			val[0] = 2;
+			val[1] = largeGroupRank; //rank of pair
+			val[2] = orderedRanks[0];
+			val[3] = orderedRanks[1];
+			val[4] = orderedRanks[2];
 		}
 
-		if (sameCards==2 && sameCards2==2) //two pair
-		{
-			value[0]=3;
-			value[1]= largeGroupRank>smallGroupRank ? largeGroupRank : smallGroupRank; //rank of greater pair
-			value[2]= largeGroupRank<smallGroupRank ? largeGroupRank : smallGroupRank;
-			value[3]=orderedRanks[0];  //extra card
+		if (sameCards == 2 && sameCards2 == 2) { //two pair
+			val[0] = 3;
+			val[1] = largeGroupRank > smallGroupRank ? largeGroupRank : smallGroupRank; //rank of greater pair
+			val[2] = largeGroupRank < smallGroupRank ? largeGroupRank : smallGroupRank;
+			val[3] = orderedRanks[0];  //extra card
 		}
 
-		if (sameCards==3 && sameCards2!=2)
-		{
-			value[0]=4;
-			value[1]= largeGroupRank;
-			value[2]=orderedRanks[0];
-			value[3]=orderedRanks[1];
+		if (sameCards == 3 && sameCards2 != 2) {
+			val[0] = 4;
+			val[1] = largeGroupRank;
+			val[2] = orderedRanks[0];
+			val[3] = orderedRanks[1];
 		}
 
-		if (straight && !flush)
-		{
-			value[0]=5;
-			value[1]=topStraightValue;
+		if (straight && !flush) {
+			val[0] = 5;
+			val[1] = topStraightValue;
 		}
 
-		if (flush && !straight)
-		{
-			value[0]=6;
-			value[1]=orderedRanks[0]; //tie determined by ranks of cards
-			value[2]=orderedRanks[1];
-			value[3]=orderedRanks[2];
-			value[4]=orderedRanks[3];
-			value[5]=orderedRanks[4];
+		if (flush && !straight) {
+			val[0] = 6;
+			val[1] = orderedRanks[0]; //tie determined by ranks of cards
+			val[2] = orderedRanks[1];
+			val[3] = orderedRanks[2];
+			val[4] = orderedRanks[3];
+			val[5] = orderedRanks[4];
 		}
 
-		if (sameCards==3 && sameCards2==2)
-		{
-			value[0]=7;
-			value[1]=largeGroupRank;
-			value[2]=smallGroupRank;
+		if (sameCards == 3 && sameCards2 == 2) {
+			val[0] = 7;
+			val[1] = largeGroupRank;
+			val[2] = smallGroupRank;
 		}
 
-		if (sameCards==4)
-		{
-			value[0]=8;
-			value[1]=largeGroupRank;
-			value[2]=orderedRanks[0];
+		if (sameCards == 4) {
+			val[0] = 8;
+			val[1] = largeGroupRank;
+			val[2] = orderedRanks[0];
 		}
 
-		if (straight && flush)
-		{
-			value[0]=9;
-			value[1]=topStraightValue;
+		if (straight && flush){
+			val[0] = 9;
+			val[1] = topStraightValue;
 		}
-
-
 	}
 
 
-	void display()
-	{
-		String s;
-		switch( value[0] )
-		{
+	public void display(int[] val) {
+		String str;
+		switch(val[0]) {
 
 		case 1:
-			s="high card";
+			str = "high card";
 			break;
 		case 2:
-			s="pair of " + Card.rankAsString(value[1]) + "\'s";
+			str = "pair of " + Card.rankAsString(val[1]) + "\'s";
 			break;
 		case 3:
-			s="two pair " + Card.rankAsString(value[1]) + " " + Card.rankAsString(value[2]);
+			str = "two pair " + Card.rankAsString(val[1]) + " " + Card.rankAsString(val[2]);
 			break;
 		case 4:
-			s="three of a kind " + Card.rankAsString(value[1]) + "\'s";
+			str = "three of a kind " + Card.rankAsString(val[1]) + "\'s";
 			break;
 		case 5:
-			s=Card.rankAsString(value[1]) + " high straight";
+			str = Card.rankAsString(val[1]) + " high straight";
 			break;
 		case 6:
-			s="flush";
+			str = "flush";
 			break;
 		case 7:
-			s="full house " + Card.rankAsString(value[1]) + " over " + Card.rankAsString(value[2]);
+			str = "full house " + Card.rankAsString(val[1]) + " over " + Card.rankAsString(val[2]);
 			break;
 		case 8:
-			s="four of a kind " + Card.rankAsString(value[1]);
+			str = "four of a kind " + Card.rankAsString(val[1]);
 			break;
 		case 9:
-			s="straight flush " + Card.rankAsString(value[1]) + " high";
+			str = "straight flush " + Card.rankAsString(val[1]) + " high";
 			break;
 		default:
-			s="error in Hand.display: value[0] contains invalid value";
+			str = "error in Hand.display: value[0] contains invalid value";
 		}
-		s = "				" + s;
-		System.out.println(s);
+		//s = "				" + str;
+		System.out.println(str);
 	}
 
 
@@ -208,9 +203,9 @@ public class Hand {
 	}
 	public int compareTo(Card[] hand1, Card[] hand2) {
 		for (int i = 0; i < 6; i++) { //loops through values to compare
-			if (this.val[i] > that.val[i]) {
+			if (val1[i] > val2[i]) {
 				return 1;
-			} else if (this.val[i] < that.val[i]) {
+			} else if (val1[i] < val2[i]) {
 				return -1;
 			}
 		}
